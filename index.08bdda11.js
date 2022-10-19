6252857bc4f375a2727615e92a7e4fda6d974a5e
aspirantmpm <110274028+aspirantmpm@users.noreply.github.com>
2022-10-19 22:36:44 +0000

    Deploying to gh-pages from @ aspirantmpm/brain_breakers@4e9eca6a04a219d03140acf507d219bc341e2b77 🚀

---
+++ index.08bdda11.js
@@ -0,0 +1,2 @@
+$(".opacity").slick({infinite:!0,arrows:!1,fade:!0,slidesToShow:1,autoplay:!0,centerMode:!0,autoplaySpeed:2e3,draggable:!1}),$(".one-time").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,arrows:!1,autoplay:!0,autoplaySpeed:9e3});
+//# sourceMappingURL=index.08bdda11.js.map
--- index.7b30a867.js
+++
@@ -1,2 +0,0 @@
-$(".opacity").slick({infinite:!0,arrows:!1,fade:!0,slidesToShow:1,autoplay:!0,centerMode:!0,autoplaySpeed:2e3,draggable:!1}),$(".one-time").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,arrows:!1});
-//# sourceMappingURL=index.7b30a867.js.map
