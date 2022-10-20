5b32228303d8e2091f107cf3fb7c60e5b7f432be
aspirantmpm <110274028+aspirantmpm@users.noreply.github.com>
2022-10-20 11:55:46 +0000

    Deploying to gh-pages from @ aspirantmpm/brain_breakers@9ab6125ba6a0406f260370ea1ff4a078ce8cf207 🚀

---
+++ index.d1aac9dd.js
@@ -0,0 +1,2 @@
+var btn=$("#button");$(window).scroll((function(){$(window).scrollTop()>300?btn.addClass("show"):btn.removeClass("show")})),$("#button").click((function(){return $("html, body").animate({scrollTop:0},100),!1}));
+//# sourceMappingURL=index.d1aac9dd.js.map
